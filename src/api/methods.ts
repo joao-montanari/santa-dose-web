c40d99385b1e2f57e7953d480d3c09103936688d
joao-montanari <joao.unipbr@gmail.com>
2025-05-22 13:44:10 -0300

    :bug: fix: Coloca o baseURL dentro do arquivo .env

--- src/api/methods.ts
+++ src/api/methods.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     // baseURL: "https://fastapi-adega.onrender.com",
-    baseURL: "http://localhost:8000"
+    baseURL: import.meta.env.VITE_BASE_BACKEND_URL
 });
 
 export async function selectMethod(
