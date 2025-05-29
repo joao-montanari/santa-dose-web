0b977b711dc887654a8bbcd2050505741591a3e6
EricBritto14 <Eric.Britto22@gmail.com>
2025-05-29 16:05:32 -0300

    :hammer: fix: Returning the correct url

--- src/api/services/getPhotoUser.ts
+++ src/api/services/getPhotoUser.ts
@@ -2,8 +2,8 @@ export default function getPhotoUser(){
     const userString = localStorage.getItem("user")
     const user = userString ? JSON.parse(userString) : null;
 
-    const baseURL = "http://localhost:8000/static"
-    // const baseURL ="https://fastapi-adega.onrender.com/static"
+    // const baseURL = "http://localhost:8000/static"
+    const baseURL ="https://fastapi-adega.onrender.com/static"
     
     const profileImageURL = `${baseURL}/${user.profile_image}`;
     
