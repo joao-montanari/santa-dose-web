96e3f9f17f41cb5a076a870f4924a08e19044eed
joao-montanari <joao.unipbr@gmail.com>
2025-06-02 09:52:12 -0300

    :bug: fix: Remove variável de ambiente desnecessária

--- src/api/services/getPhotoUser.ts
+++ src/api/services/getPhotoUser.ts
@@ -2,7 +2,7 @@ export default function getPhotoUser(){
     const userString = localStorage.getItem("user")
     const user = userString ? JSON.parse(userString) : null;
 
-    const baseURL = import.meta.env.VITE_BACKEND_URL_STATIC
+    const baseURL = `${import.meta.env.VITE_BACKEND_URL}/static`
     
     const profileImageURL = `${baseURL}/${user.profile_image}`;
     
