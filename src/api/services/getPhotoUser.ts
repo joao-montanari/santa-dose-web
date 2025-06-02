e0f8132009d40c3cac5b439d2bf1a8c94319bdb4
joao-montanari <joao.unipbr@gmail.com>
2025-06-01 23:11:50 -0300

    :recycle: refactor: Adiciona a URL de static as variáveis de ambiente

--- src/api/services/getPhotoUser.ts
+++ src/api/services/getPhotoUser.ts
@@ -2,8 +2,7 @@ export default function getPhotoUser(){
     const userString = localStorage.getItem("user")
     const user = userString ? JSON.parse(userString) : null;
 
-    // const baseURL = "http://localhost:8000/static"
-    const baseURL ="https://fastapi-adega.onrender.com/static"
+    const baseURL = import.meta.env.VITE_BACKEND_URL_STATIC
     
     const profileImageURL = `${baseURL}/${user.profile_image}`;
     
