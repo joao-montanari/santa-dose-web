35821a072f1badc9e82a010565e6a61fd10ef64e
EricBritto14 <Eric.Britto22@gmail.com>
2025-06-13 14:26:17 -0300

    :sparkles: feat: Updating the bills page, by the request of the owner

--- src/components/TableSales/index.tsx
+++ src/components/TableSales/index.tsx
@@ -8,10 +8,13 @@ const TableSales = (
       salesColumTitle,
       totalLabel,
       daysInMonth,
+      nameAccount,
       onDayValueChange,
       titleNamesSpun,
       onTotalChange,
       initialSales,
+      sales,
+      setSales
 
     }
      : {
@@ -20,15 +23,15 @@ const TableSales = (
         salesColumTitle ?: string,
         totalLabel?: string,
         daysInMonth : number,
+        nameAccount?: string,
         titleNamesSpun?: string, 
         onDayValueChange ?: (day: number, value: number) => void
         onTotalChange?: (total: number) => void,
-        initialSales?: {dia: string, mes: string, valor: string}[];
+        initialSales?: {dia: string, mes: string, valor: string, motivo: string}[];
+        sales: {day : string; value: string; reason: string}[],
+        setSales: React.Dispatch<React.SetStateAction<{ day: string; value: string; reason: string}[]>>
     })  =>{
     //Estado para armazenar os valores de cada dia do mês
-    const [sales, setSales] = useState<{ day: string; value: string }[]>([]);
-
-    
 
     useEffect(() => {
         if (initialSales && daysInMonth > 0) {
@@ -36,6 +39,7 @@ const TableSales = (
                 const existingValue = initialSales.find(item => Number(item.dia) === i + 1);
                 return {
                     day: (i + 1).toString(),
+                    reason: existingValue ? existingValue.motivo : "",
                     value: existingValue ? existingValue.valor : "", 
                 };
             });
@@ -43,28 +47,34 @@ const TableSales = (
             console.log("Valores carregados em TableSales:", newSales);
             setSales(newSales);
         }
-    }, [initialSales, daysInMonth]); 
+    }, [ daysInMonth]); 
     //Mudando os valores nos inputs
-    const handleInputChange = (index: number, value: string) => {
-        const sanitizedValue = value.replace(/[^0-9.]/g, "");
+    const handleInputChange = (index: number, field: "value" | "reason" | "day", inputValue: string) => {
 
         setSales((prevSales) => {
             const updatedSales = [...prevSales];
-            updatedSales[index] = { ...updatedSales[index], value: sanitizedValue };
 
-            const numericValue = sanitizedValue === "" ? 0 : parseInt(sanitizedValue, 10);
+            let sanitizedValue = inputValue;
+            if(field === "value"){
+                const sanitizedValue = inputValue.replace(/[^0-9.]/g, "");
+            }
 
+            updatedSales[index] = { ...updatedSales[index], [field]: sanitizedValue,};
+
+            
+            if(field === "value"){
+                    const numericValue = sanitizedValue === "" ? 0 : parseInt(sanitizedValue, 10);
+                    setTimeout(() => {
+                    if (onDayValueChange) {
+                        onDayValueChange(index + 1, numericValue);
+                    }
+                    if (onTotalChange) {
+                        const total = calculateTotal(updatedSales);
+                        onTotalChange(total);
+                    }
+                }, 0);
+            }
             // Aguarde a renderização antes de atualizar o estado do pai
-            setTimeout(() => {
-                if (onDayValueChange) {
-                    onDayValueChange(index + 1, numericValue);
-                }
-                if (onTotalChange) {
-                    const total = calculateTotal(updatedSales);
-                    onTotalChange(total);
-                }
-            }, 0);
-
             return updatedSales;
         });
           
@@ -106,6 +116,7 @@ const TableSales = (
                     <thead>
                         <tr>
                             <th>{dayColumnTitle}</th>
+                            {nameAccount && <th>{nameAccount}</th>}                            
                             {titleNamesSpun && <th>{titleNamesSpun}</th>}
                             <th>{salesColumTitle}</th>
                         </tr>
@@ -114,8 +125,9 @@ const TableSales = (
                         {sales.map((sale, i) => (
                             <tr key={i}>
                                 <td>{i + 1}</td>
-                                {titleNamesSpun && <td><input className="input-style-table" type="text" value={sale.day} onChange={(e) => handleInputChange(i, e.target.value)} /></td>}
-                                <td><input className="input-style-table" value={sale.value || 0} onChange={(e) => handleInputChange(i, e.target.value)} /></td>
+                                {titleNamesSpun && <td><input className="input-style-table" type="text" value={sale.day} onChange={(e) => handleInputChange(i, "day", e.target.value)} /></td>}
+                                {nameAccount && <td><input className="input-style-table" type="text" value={sale.reason} onChange={(e) => handleInputChange(i, "reason", e.target.value)} /></td>}
+                                <td><input className="input-style-table" value={sale.value || 0} onChange={(e) => handleInputChange(i, "value", e.target.value)} /></td>
                             </tr>
                         ))}
                         <tr>
--- src/pages/Bills/index.tsx
+++ src/pages/Bills/index.tsx
@@ -16,9 +16,10 @@ import getPhotoUser from "@Api/services/getPhotoUser";
 
 const Bills = () =>{
     const navigate = useNavigate()
-    const [product, setProduct] = useState({tipo: '', dia: 0});
+    const [product, setProduct] = useState({tipo: '', dia: 0, motivo: ''});
     const [loading, setLoading] = useState(false)
     const [monthsTotal, setMonthsTotal] = useState<Record<string, number>>({});
+    const [sales, setSales] = useState<{ day: string; value: string; reason: string }[]>([]);
     
     const [note, setNote] = useState<NotificationType>({
         message: "",
@@ -74,7 +75,9 @@ const Bills = () =>{
                 .filter(([key]) => key.startsWith(`${product.tipo}-`)) // Filtra apenas os valores do mês selecionado
                 .map(([key, valor]) => {
                     const dia = key.split("-")[1]; // Extrai o dia da chave "mes-dia"
-                    return { mes: product.tipo, dia: Number(dia), valor };
+                    const sale = sales.find((s) => s.day === dia);
+                    const reason = sale?.reason || "";
+                    return { mes: product.tipo, dia: Number(dia), valor, motivo: reason};
                 });
 
     
@@ -174,21 +177,34 @@ const Bills = () =>{
         setLoading(true)
         try{
             const response = await getDaysBillsValue(product.tipo);
-            console.log("Valores de initialSales antes de passar para a tabela: ", Object.entries(monthsTotal)
-                .filter(([key]) => key.startsWith(`${product.tipo}-`)) // Filtra apenas os valores do mês selecionado
-                .map(([key, valor]) => ({
-                    dia: key.split("-")[1], // Extrai o dia da chave "mes-dia"
-                    mes: product.tipo, // Usa o mês selecionado 
-                    valor: valor ? valor.toString() : "0", 
-            })))
-            if(response){
-                const novosValores: Record<string, number> = {};
-                response[1].forEach((registro: {dia: number, valor: number}) => {
-                novosValores[`${product.tipo}-${registro.dia}`] = registro.valor;
+            const novosSales: { day: string; value: string; reason: string}[] = [];
+            const novosValores: Record<string, number> = {};
+
+            for(let i = 1; i <= 31; i++){
+                const registro = response[1]?.find((r: {dia : number}) => r.dia === i);
+
+                // response[1].forEach((registro: {dia: number, valor: number, motivo?: string}) => {
+                    // novosValores[`${product.tipo}-${registro.dia}`] = registro.valor;
+                novosSales.push({
+                        day: i.toString(),
+                        value: registro ? registro.valor.toString() : "",
+                        reason: registro ? registro.motivo || "" : "",
                 });
 
+                if(registro){
+                    novosValores[`${product.tipo}-${i}`] = registro.valor;
+                    }
+                }
+
+                setSales(novosSales)
                 setMonthsTotal(novosValores)
-        }
+
+            // console.log("Valores de initialSales antes de passar para a tabela: ", Object.entries(monthsTotal)
+            //     .filter(([key]) => key.startsWith(`${product.tipo}-`)) // Filtra apenas os valores do mês selecionado
+            //     .map(([key, valor]) => ({
+            //         dia: key.split("-")[1], // Extrai o dia da chave "mes-dia"
+            //         mes: product.tipo, // Usa o mês selecionado 
+            //         valor: valor ? valor.toString() : "0", 
         }catch (error){
             setNote({
                 message: "Erro ao carregar os valores do mês",
@@ -235,6 +251,7 @@ const Bills = () =>{
                 salesColumTitle="Valores Boletos"
                 totalLabel="Total Do Mês"
                 daysInMonth={31}
+                nameAccount="Nome Boleto"
                 onTotalChange={handleTotalChange}
                 onDayValueChange={handleDayValueChange}
                 initialSales={Object.entries(monthsTotal)
@@ -243,7 +260,10 @@ const Bills = () =>{
                         dia: key.split("-")[1], // Extrai o dia da chave "mes-dia"
                         mes: product.tipo, // Usa o mês selecionado 
                         valor: valor ? valor.toString() : "0", 
+                        motivo: "",
                     }))}
+                sales={sales}
+                setSales={setSales}
              />
 
             <div id="buttons-align">
